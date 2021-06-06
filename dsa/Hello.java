bd1889750dc0bc5b3a747fe892f00d72d3cd9767
virajbhatnagar <virajwednesday@gmail.com>
2021-06-06 22:04:10 +0530

    change

--- dsa/Hello.java
+++ dsa/Hello.java
@@ -2,12 +2,19 @@ import java.io.*;
 import java.util.*;
 class Hello{
 
+
+	public static void printName(){
+		System.out.println("Sherlock");
+	}
+
 	public static void main(String[] args) throws IOException{
 
-		BufferedReader reader = new BufferedReader(new FileReader("inputs"));
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 		String name = reader.readLine();
 		System.out.println(name);
 		reader.close();
+
+		Hello.printName();
 
 
 	}
