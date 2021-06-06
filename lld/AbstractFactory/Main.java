184d09258ae8508195084b9fc1e63195d3345da7
Viraj Bhatnagar <virajwednesday@gmail.com>
2021-06-06 23:40:33 +0530

    Update Main.java

--- lld/AbstractFactory/Main.java
+++ lld/AbstractFactory/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
         FileReader fileReader = new XMLReader();
-        fileReader.readFile("aditi.json");
+        fileReader.readFile("temp.json");
     }
 
 }
